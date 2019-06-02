df38b9144ff1cb00b4482a07899667d6fc3a999b
tougodo <corbin.clement@gmail.com>
2019-06-02 14:10:21 +0200

    1559477421

--- audio.js
+++ audio.js
@@ -19,15 +19,28 @@ document.getElementById("source_audio").addEventListener("change",async function
 	}
 	if (e.target.value == "audio_in")
 	{
+		document.getElementById("l_osc_type").classList.add("hidden")
+		document.getElementById("l_osc_freq").classList.add("hidden")
+		document.getElementById("osc_type").classList.add("hidden")
+		document.getElementById("osc_freq").classList.add("hidden")
+		document.getElementById("l_file_input").classList.add("hidden")
+		document.getElementById("file_input").classList.add("hidden")
 		wa.type = e.target.value;
 		wa.stream = await get_mic();
 		wa.audioSourceNode = wa.audioCtx.createMediaStreamSource(wa.stream);
+		
 	}
 	else if (e.target.value == "file")
 	{
+		document.getElementById("l_osc_type").classList.add("hidden")
+		document.getElementById("l_osc_freq").classList.add("hidden")
+		document.getElementById("osc_type").classList.add("hidden")
+		document.getElementById("osc_freq").classList.add("hidden")
+		document.getElementById("l_file_input").classList.remove("hidden")
+		document.getElementById("file_input").classList.remove("hidden")
 		wa.type = e.target.value;
 		wa.stream = new Audio();
-		wa.stream.src = '1.mp3';
+		wa.stream.src = await get_input();
 		wa.stream.autoplay = true;
 		wa.stream.preload = 'auto';
 		wa.audioSourceNode = wa.audioCtx.createMediaElementSource(wa.stream);
@@ -36,9 +49,15 @@ document.getElementById("source_audio").addEventListener("change",async function
 	{
 		wa.type = "osc";
 		wa.audioSourceNode = wa.audioCtx.createOscillator();
-		wa.audioSourceNode.type = 'sine';
-		wa.audioSourceNode.frequency.setValueAtTime(16, wa.audioCtx.currentTime);
+		wa.audioSourceNode.type = document.getElementById("osc_type").value;
+		wa.audioSourceNode.frequency.setValueAtTime(document.getElementById("osc_freq").value, wa.audioCtx.currentTime);
 		wa.audioSourceNode.start();
+		document.getElementById("l_osc_type").classList.remove("hidden")
+		document.getElementById("l_osc_freq").classList.remove("hidden")
+		document.getElementById("osc_type").classList.remove("hidden")
+		document.getElementById("osc_freq").classList.remove("hidden")
+		document.getElementById("l_file_input").classList.add("hidden")
+		document.getElementById("file_input").classList.add("hidden")
 	}
 	wa.analyserNode = wa.audioCtx.createAnalyser();
 	wa.analyserNode.fftSize = 1024;
@@ -47,7 +66,14 @@ document.getElementById("source_audio").addEventListener("change",async function
 	const dataArray = new Uint8Array(bufferLength);
 
 	wa.audioSourceNode.connect(wa.analyserNode);
-	//analyserNode.connect(audioCtx.destination);
+	if (document.getElementById("out_monitor").checked && wa.type != "audio_in")
+	{
+		wa.analyserNode.connect(wa.audioCtx.destination);
+	}
+	else
+	{
+		document.getElementById("out_monitor").checked = false;
+	}
 
 	draw();
 	async function draw() {
@@ -66,6 +92,45 @@ function get_mic()
 		})
 }
 window.onbeforeunload = wa.audioCtx.close;
+document.getElementById("osc_freq").addEventListener("change",function (e)
+{
+	wa.audioSourceNode.frequency.setValueAtTime(e.target.value, wa.audioCtx.currentTime);
+})
+document.getElementById("osc_type").addEventListener("change",function (e)
+{
+	wa.audioSourceNode.type = e.target.value;
+})
+function get_input()
+{
+	return new Promise(function(resolve,reject){
+		document.getElementById("file_input").addEventListener("change",async function (e)
+		{
+			src = await read(e.target.files[0]);
+			resolve(src)
+		})
+	})
+}
+document.getElementById("out_monitor").addEventListener("change",async function (e)
+{
+	if (e.target.checked)
+	{
+		wa.analyserNode.connect(wa.audioCtx.destination);
+	}
+	else
+	{
+		wa.analyserNode.disconnect();
+	}
+})
+function read(what)
+{
+	return new Promise(function(resolve,reject){
+		var reader = new FileReader();
+		reader.addEventListener('load', function() {
+			resolve(this.result)
+		});
+		reader.readAsDataURL(what);
+	})
+}
 //Create audio source
 /*
 var audioSourceNode = audioCtx.createOscillator();
