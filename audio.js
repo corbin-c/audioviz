77754c1fe8b231c385e80147b17bd7e8f2fbd26f
Clément Corbin <corbin.clement@gmail.com>
2019-09-23 18:52:35 +0200

    lint

--- audio.js
+++ audio.js
@@ -1,182 +1,156 @@
-function WebAudio()
-{
-	this.type = "";
-	this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-	this.stream = {};
-	this.audioSourceNode = {};
-	this.analyserNode = {};
+function WebAudio() {
+  this.type = "";
+  this.audioCtx = new (window.AudioContext ||
+                      window.webkitAudioContext)();
+  this.stream = {};
+  this.audioSourceNode = {};
+  this.analyserNode = {};
 }
 wa = new WebAudio();
 document.getElementById("buff_size").value = 512;
-document.getElementById("source_audio").addEventListener("change",async function (e) {
-	try
-	{
-		wa.audioSourceNode.disconnect();
-		wa.analyserNode.disconnect()
-	}
-	catch
-	{
-		console.log("could not disconnect audio node");
-	}
-	if (e.target.value == "audio_in")
-	{
-		document.getElementById("l_osc_type").classList.add("hidden")
-		document.getElementById("l_osc_freq").classList.add("hidden")
-		document.getElementById("osc_type").classList.add("hidden")
-		document.getElementById("osc_freq").classList.add("hidden")
-		document.getElementById("l_file_input").classList.add("hidden")
-		document.getElementById("file_input").classList.add("hidden")
-		wa.type = e.target.value;
-		wa.stream = await get_mic();
-		wa.audioSourceNode = wa.audioCtx.createMediaStreamSource(wa.stream);
-		
-	}
-	else if (e.target.value == "file")
-	{
-		document.getElementById("l_osc_type").classList.add("hidden")
-		document.getElementById("l_osc_freq").classList.add("hidden")
-		document.getElementById("osc_type").classList.add("hidden")
-		document.getElementById("osc_freq").classList.add("hidden")
-		document.getElementById("l_file_input").classList.remove("hidden")
-		document.getElementById("file_input").classList.remove("hidden")
-		wa.type = e.target.value;
-		wa.stream = new Audio();
-		wa.stream.src = await get_input();
-		wa.stream.autoplay = true;
-		wa.stream.preload = 'auto';
-		wa.audioSourceNode = wa.audioCtx.createMediaElementSource(wa.stream);
-	}
-	else
-	{
-		wa.type = "osc";
-		wa.audioSourceNode = wa.audioCtx.createOscillator();
-		wa.audioSourceNode.type = document.getElementById("osc_type").value;
-		wa.audioSourceNode.frequency.setValueAtTime(document.getElementById("osc_freq").value, wa.audioCtx.currentTime);
-		wa.audioSourceNode.start();
-		document.getElementById("l_osc_type").classList.remove("hidden")
-		document.getElementById("l_osc_freq").classList.remove("hidden")
-		document.getElementById("osc_type").classList.remove("hidden")
-		document.getElementById("osc_freq").classList.remove("hidden")
-		document.getElementById("l_file_input").classList.add("hidden")
-		document.getElementById("file_input").classList.add("hidden")
-	}
-	wa.analyserNode = wa.audioCtx.createAnalyser();
-	wa.analyserNode.fftSize = document.getElementById("buff_size").value;
-	svg.init_graph(wa.analyserNode.fftSize)
-	wa.bufferLength = wa.analyserNode.fftSize;
-	wa.dataArray = new Uint8Array(wa.bufferLength);
+document.getElementById("source_audio")
+  .addEventListener("change",async function (e) {
+    try {
+      wa.audioSourceNode.disconnect();
+      wa.analyserNode.disconnect()
+    }
+    catch {
+      console.log("could not disconnect audio node");
+    }
+    if (e.target.value == "audio_in") {
+      document.getElementById("l_osc_type").classList.add("hidden")
+      document.getElementById("l_osc_freq").classList.add("hidden")
+      document.getElementById("osc_type").classList.add("hidden")
+      document.getElementById("osc_freq").classList.add("hidden")
+      document.getElementById("l_file_input").classList.add("hidden")
+      document.getElementById("file_input").classList.add("hidden")
+      wa.type = e.target.value;
+      wa.stream = await get_mic();
+      wa.audioSourceNode = wa.audioCtx
+        .createMediaStreamSource(wa.stream);
+      
+    } else if (e.target.value == "file") {
+      document.getElementById("l_osc_type").classList.add("hidden")
+      document.getElementById("l_osc_freq").classList.add("hidden")
+      document.getElementById("osc_type").classList.add("hidden")
+      document.getElementById("osc_freq").classList.add("hidden")
+      document.getElementById("l_file_input").classList.remove("hidden")
+      document.getElementById("file_input").classList.remove("hidden")
+      wa.type = e.target.value;
+      wa.stream = new Audio();
+      wa.stream.src = await get_input();
+      wa.stream.autoplay = true;
+      wa.stream.preload = 'auto';
+      wa.audioSourceNode = wa.audioCtx
+        .createMediaElementSource(wa.stream);
+    } else {
+      wa.type = "osc";
+      wa.audioSourceNode = wa.audioCtx.createOscillator();
+      wa.audioSourceNode.type = document.getElementById("osc_type")
+        .value;
+      wa.audioSourceNode.frequency.setValueAtTime(
+        document.getElementById("osc_freq").value,
+        wa.audioCtx.currentTime);
+      wa.audioSourceNode.start();
+      document.getElementById("l_osc_type").classList.remove("hidden")
+      document.getElementById("l_osc_freq").classList.remove("hidden")
+      document.getElementById("osc_type").classList.remove("hidden")
+      document.getElementById("osc_freq").classList.remove("hidden")
+      document.getElementById("l_file_input").classList.add("hidden")
+      document.getElementById("file_input").classList.add("hidden")
+    }
+    wa.analyserNode = wa.audioCtx.createAnalyser();
+    wa.analyserNode.fftSize = document.getElementById("buff_size").value;
+    svg.init_graph(wa.analyserNode.fftSize)
+    wa.bufferLength = wa.analyserNode.fftSize;
+    wa.dataArray = new Uint8Array(wa.bufferLength);
 
-	wa.audioSourceNode.connect(wa.analyserNode);
-	if (document.getElementById("out_monitor").checked && wa.type != "audio_in")
-	{
-		wa.analyserNode.connect(wa.audioCtx.destination);
-	}
-	else
-	{
-		document.getElementById("out_monitor").checked = false;
-	}
-	if (!svg.drawing("status"))
-	{
-		draw();
-		async function draw() {
-			wa.analyserNode.getByteTimeDomainData(wa.dataArray);
-			svg.fourier(wa.dataArray)
-			await incr_wait(0,(wa.analyserNode.fftSize/wa.audioCtx.sampleRate)*1000)
-			draw();
-		}
-	}
+    wa.audioSourceNode.connect(wa.analyserNode);
+    if (document.getElementById("out_monitor").checked
+      && wa.type != "audio_in") {
+      wa.analyserNode.connect(wa.audioCtx.destination);
+    } else {
+      document.getElementById("out_monitor").checked = false;
+    }
+    if (!svg.drawing("status")) {
+      draw();
+      async function draw() {
+        wa.analyserNode.getByteTimeDomainData(wa.dataArray);
+        svg.fourier(wa.dataArray)
+        await incr_wait(0,
+          (wa.analyserNode.fftSize/wa.audioCtx.sampleRate)*1000)
+        draw();
+      }
+    }
 })
-
-function get_mic()
-{
-	return new Promise(function(resolve,reject){
-	navigator.mediaDevices.getUserMedia ({audio: true, video: false})
-		.then(async function(stream) {resolve(stream)})
-		})
+function get_mic() {
+  return new Promise(function(resolve,reject){
+  navigator.mediaDevices.getUserMedia ({audio: true, video: false})
+    .then(async function(stream) {resolve(stream)})
+    })
 }
-window.onbeforeunload = wa.audioCtx.close;
-document.getElementById("osc_freq").addEventListener("change",function (e)
-{
-	wa.audioSourceNode.frequency.setValueAtTime(e.target.value, wa.audioCtx.currentTime);
+document.getElementById("osc_freq").addEventListener("change",
+  function (e) {
+  wa.audioSourceNode.frequency
+    .setValueAtTime(e.target.value, wa.audioCtx.currentTime);
 })
-document.getElementById("osc_type").addEventListener("change",function (e)
-{
-	wa.audioSourceNode.type = e.target.value;
+document.getElementById("osc_type").addEventListener("change",
+  function (e) {
+  wa.audioSourceNode.type = e.target.value;
 })
-document.getElementById("pt_size").addEventListener("change",function (e)
-{
-	svg.init_graph(wa.analyserNode.fftSize)
+document.getElementById("pt_size").addEventListener("change",
+  function (e) {
+  svg.init_graph(wa.analyserNode.fftSize)
 })
-document.getElementById("buff_size").addEventListener("change",function (e)
-{
-	bufsize = (wa.analyserNode.fftSize || 512);
-	if (e.target.value != bufsize)
-	{
-		bufsize = pow_two(bufsize);
-		bufsize = (wa.analyserNode.fftSize > e.target.value) ? Math.pow(2,bufsize-1):Math.pow(2,bufsize+1);
-		try
-		{
-			svg.init_graph(bufsize)
-			e.target.value = bufsize;
-			wa.analyserNode.fftSize = bufsize;
-			wa.bufferLength = wa.analyserNode.fftSize;
-			wa.dataArray = new Uint8Array(wa.bufferLength);
-		}
-		catch
-		{
-			console.log("buffersize failure");
-		}
-	}
+document.getElementById("buff_size").addEventListener("change",
+  function (e) {
+  bufsize = (wa.analyserNode.fftSize || 512);
+  if (e.target.value != bufsize) {
+    bufsize = pow_two(bufsize);
+    bufsize = (wa.analyserNode.fftSize > e.target.value) ?
+      Math.pow(2,bufsize-1):Math.pow(2,bufsize+1);
+    try {
+      svg.init_graph(bufsize)
+      e.target.value = bufsize;
+      wa.analyserNode.fftSize = bufsize;
+      wa.bufferLength = wa.analyserNode.fftSize;
+      wa.dataArray = new Uint8Array(wa.bufferLength);
+    }
+    catch {
+      console.log("buffersize failure");
+    }
+  }
 })
-function get_input()
-{
-	return new Promise(function(resolve,reject){
-		document.getElementById("file_input").addEventListener("change",async function (e)
-		{
-			src = await read(e.target.files[0]);
-			resolve(src)
-		})
-	})
+function get_input() {
+  return new Promise(function(resolve,reject){
+    document.getElementById("file_input").addEventListener("change",
+      async function (e) {
+      src = await read(e.target.files[0]);
+      resolve(src)
+    })
+  })
 }
-document.getElementById("out_monitor").addEventListener("change",async function (e)
-{
-	if (e.target.checked && wa.type != "audio_in")
-	{
-		wa.analyserNode.connect(wa.audioCtx.destination);
-	}
-	else
-	{
-		wa.analyserNode.disconnect();
-	}
+document.getElementById("out_monitor").addEventListener("change",
+  async function (e) {
+  if (e.target.checked && wa.type != "audio_in") {
+    wa.analyserNode.connect(wa.audioCtx.destination);
+  } else {
+    wa.analyserNode.disconnect();
+  }
 })
-function read(what)
-{
-	return new Promise(function(resolve,reject){
-		var reader = new FileReader();
-		reader.addEventListener('load', function() {
-			resolve(this.result)
-		});
-		reader.readAsDataURL(what);
-	})
+function read(what) {
+  return new Promise(function(resolve,reject){
+    let reader = new FileReader();
+    reader.addEventListener('load', function() {
+      resolve(this.result)
+    });
+    reader.readAsDataURL(what);
+  })
 }
-function pow_two(number)
-{
-	var i = 1;
-	while (Math.pow(2,i)<number)
-	{
-		i++;
-	}
-	return i;
+function pow_two(number) {
+  let i = 1;
+  while (Math.pow(2,i)<number) {
+    i++;
+  }
+  return i;
 }
-//Create audio source
-/*
-var audioSourceNode = audioCtx.createOscillator();
-audioSourceNode.type = 'sine';
-audioSourceNode.frequency.setValueAtTime(4400, audioCtx.currentTime); // value in hertz
-audioSourceNode.start();
-
-const stream = new Audio();
-stream.src = '3.flac';//insert file name here
-stream.autoplay = true;
-stream.preload = 'auto';
-*/
--- graph.js
+++ graph.js
@@ -1,152 +1,141 @@
-var svgns = 'http://www.w3.org/2000/svg';
-function Svg(min_x,min_y,max_x,max_y)
-{
-	this.min_x = min_x;
-	this.min_y = min_y;
-	this.max_x = max_x;
-	this.max_y = max_y;
-	this.delta_x = max_x-min_x;
-	this.delta_y = max_y-min_y;
-	this.drawing_status = false;
-	this.id = "svg"+new Date().valueOf();
-	this.svg = document.createElementNS(svgns, "svg");
-	this.svg.setAttribute("style","transform: scale(1,-1);")
-	this.svg.setAttribute("id", this.id)
-	this.svg.setAttribute("xmlns", svgns)	
-	this.svg.setAttribute('xmlns:xlink', "http://www.w3.org/1999/xlink")
-	this.svg.setAttributeNS(null, 'viewBox', min_x+" "+min_y+" "+this.delta_x+" "+this.delta_y);
-	document.querySelector("section").append(this.svg);
+const svgns = "http://www.w3.org/2000/svg";
+let svg = new Svg(-500,-500,500,500);
+function Svg(min_x,min_y,max_x,max_y) {
+  this.min_x = min_x;
+  this.min_y = min_y;
+  this.max_x = max_x;
+  this.max_y = max_y;
+  this.delta_x = max_x-min_x;
+  this.delta_y = max_y-min_y;
+  this.drawing_status = false;
+  this.id = "svg"+new Date().valueOf();
+  this.svg = document.createElementNS(svgns, "svg");
+  this.svg.setAttribute("style","transform: scale(1,-1);")
+  this.svg.setAttribute("id", this.id)
+  this.svg.setAttribute("xmlns", svgns) 
+  this.svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink")
+  this.svg.setAttributeNS(null, "viewBox", min_x+" "+min_y+" "
+    +this.delta_x+" "+this.delta_y);
+  document.querySelector("main").append(this.svg);
 }
-Svg.prototype.drawing = function(str)
-{
-	if (str == "status")
-	{
-		return this.drawing_status
-	}
-	else
-	{
-		this.drawing_status = true;
-	}
+Svg.prototype.drawing = function(str) {
+  if (str == "status") {
+    return this.drawing_status
+  } else {
+    this.drawing_status = true;
+  }
 }
-Svg.prototype.axis = function(step)
-{
-	this.step = step;
-	x = document.createElementNS(svgns, "line");
-	y = document.createElementNS(svgns, "line");
-	x.setAttributeNS(null,"x2",this.max_x);
-	x.setAttributeNS(null,"x1",this.min_x);
-	x.setAttributeNS(null,"stroke","black");
-	y.setAttributeNS(null,"y2",this.max_y);
-	y.setAttributeNS(null,"y1",this.min_y);
-	y.setAttributeNS(null,"stroke","black");
-	arrow_x = document.createElementNS(svgns, "polyline");
-	arrow_x.setAttributeNS(null,"stroke","black");
-	arrow_x.setAttributeNS(null,"fill","black");
-	arrow_y = document.createElementNS(svgns, "polyline");
-	arrow_y.setAttributeNS(null,"stroke","black");
-	arrow_y.setAttributeNS(null,"fill","black");
+Svg.prototype.axis = function(step) {
+  this.step = step;
+  x = document.createElementNS(svgns, "line");
+  y = document.createElementNS(svgns, "line");
+  x.setAttributeNS(null,"x2",this.max_x);
+  x.setAttributeNS(null,"x1",this.min_x);
+  x.setAttributeNS(null,"stroke","black");
+  y.setAttributeNS(null,"y2",this.max_y);
+  y.setAttributeNS(null,"y1",this.min_y);
+  y.setAttributeNS(null,"stroke","black");
+  arrow_x = document.createElementNS(svgns, "polyline");
+  arrow_x.setAttributeNS(null,"stroke","black");
+  arrow_x.setAttributeNS(null,"fill","black");
+  arrow_y = document.createElementNS(svgns, "polyline");
+  arrow_y.setAttributeNS(null,"stroke","black");
+  arrow_y.setAttributeNS(null,"fill","black");
 
-	arrow_x.setAttributeNS(null,"points",(this.max_x-this.delta_x/100)+","+(-this.delta_y/200)+" "+(this.max_x-this.delta_x/100)+","+(this.delta_y/200)+" "+(this.max_x)+",0");
-	arrow_y.setAttributeNS(null,"points",(-this.delta_x/200)+","+(this.max_y-this.delta_y/100)+" "+(this.delta_x/200)+","+(this.max_y-this.delta_y/100)+" 0,"+(this.max_y));
-	//svg = document.getElementById(this.id);
-	this.svg.append(x)
-	this.svg.append(y)
-	this.svg.append(arrow_x)
-	this.svg.append(arrow_y)
-	var i = this.min_x;
-	for (i=this.min_x;i<this.max_x;i=i+step)
-	{
-		vstep = document.createElementNS(svgns, "line");
-		vstep.setAttributeNS(null,"y2",(-this.delta_y/200));
-		vstep.setAttributeNS(null,"y1",(this.delta_y/200));
-		vstep.setAttributeNS(null,"x1",i);
-		vstep.setAttributeNS(null,"x2",i);
-		vstep.setAttributeNS(null,"stroke","black");
-		this.svg.append(vstep)
-	}
-	var i = this.min_y;
-	for (i=this.min_y;i<this.max_y;i=i+step)
-	{
-		vstep = document.createElementNS(svgns, "line");
-		vstep.setAttributeNS(null,"x2",(-this.delta_x/200));
-		vstep.setAttributeNS(null,"x1",(this.delta_x/200));
-		vstep.setAttributeNS(null,"y1",i);
-		vstep.setAttributeNS(null,"y2",i);
-		vstep.setAttributeNS(null,"stroke","black");
-		this.svg.append(vstep)
-	}
+  arrow_x.setAttributeNS(null,"points",(this.max_x-this.delta_x/100)+","
+  +(-this.delta_y/200)+" "+(this.max_x-this.delta_x/100)+","
+  +(this.delta_y/200)+" "+(this.max_x)+",0");
+  arrow_y.setAttributeNS(null,"points",(-this.delta_x/200)+","
+  +(this.max_y-this.delta_y/100)+" "+(this.delta_x/200)+","
+  +(this.max_y-this.delta_y/100)+" 0,"+(this.max_y));
+  //svg = document.getElementById(this.id);
+  this.svg.append(x)
+  this.svg.append(y)
+  this.svg.append(arrow_x)
+  this.svg.append(arrow_y)
+  let i = this.min_x;
+  for (i=this.min_x;i<this.max_x;i=i+step) {
+    vstep = document.createElementNS(svgns, "line");
+    vstep.setAttributeNS(null,"y2",(-this.delta_y/200));
+    vstep.setAttributeNS(null,"y1",(this.delta_y/200));
+    vstep.setAttributeNS(null,"x1",i);
+    vstep.setAttributeNS(null,"x2",i);
+    vstep.setAttributeNS(null,"stroke","black");
+    this.svg.append(vstep)
+  }
+  let i = this.min_y;
+  for (i=this.min_y;i<this.max_y;i=i+step) {
+    vstep = document.createElementNS(svgns, "line");
+    vstep.setAttributeNS(null,"x2",(-this.delta_x/200));
+    vstep.setAttributeNS(null,"x1",(this.delta_x/200));
+    vstep.setAttributeNS(null,"y1",i);
+    vstep.setAttributeNS(null,"y2",i);
+    vstep.setAttributeNS(null,"stroke","black");
+    this.svg.append(vstep)
+  }
 }
 
-Svg.prototype.draw = async function(limits_x,limits_y,step)
-{
-	var x = limits_x[0];
-	for (x=limits_x[0];x<limits_x[1];x=x+step)
-	{
-		y = Math.sin(Math.sqrt(x));
-		point = document.createElementNS(svgns, "circle");
-		point.setAttributeNS(null,"cx",x*(this.max_x/limits_x[1]));
-		point.setAttributeNS(null,"cy",y*(this.max_y/limits_y[1]));
-		point.setAttributeNS(null,"r",2);
-		point.setAttributeNS(null,"fill","#d17");
-		this.svg.append(point)
-		await incr_wait(0,0)
-	}
+Svg.prototype.draw = async function(limits_x,limits_y,step) {
+  let x = limits_x[0];
+  for (x=limits_x[0];x<limits_x[1];x=x+step) {
+    y = Math.sin(Math.sqrt(x));
+    point = document.createElementNS(svgns, "circle");
+    point.setAttributeNS(null,"cx",x*(this.max_x/limits_x[1]));
+    point.setAttributeNS(null,"cy",y*(this.max_y/limits_y[1]));
+    point.setAttributeNS(null,"r",2);
+    point.setAttributeNS(null,"fill","#d17");
+    this.svg.append(point)
+    await incr_wait(0,0)
+  }
 }
-Svg.prototype.fourier = async function(data,time)
-{
-	for (x in data)
-	{
-		var com_x = parseInt(document.getElementById("circ").value)*Math.PI*2*(x/document.getElementById("main_group").children.length);
-		var com_y = data[x];
-		com_y = data[x]*Math.sin(com_x)*1.8;
-		com_x = data[x]*Math.cos(com_x)*1.8;
-		point = document.getElementById("p_"+x)
-		point.setAttributeNS(null,"cx",com_x);
-		point.setAttributeNS(null,"cy",com_y);
-	}
+Svg.prototype.fourier = async function(data,time) {
+  for (x in data) {
+    let com_x = parseInt(document.getElementById("circ").value)*Math.PI
+      *2*(x/document.getElementById("main_group").children.length);
+    let com_y = data[x];
+    com_y = data[x]*Math.sin(com_x)*1.8;
+    com_x = data[x]*Math.cos(com_x)*1.8;
+    point = document.getElementById("p_"+x)
+    point.setAttributeNS(null,"cx",com_x);
+    point.setAttributeNS(null,"cy",com_y);
+  }
 }
-Svg.prototype.init_graph = function(items)
-{
-	if (document.getElementById("main_group") != null)
-	{
-		group = document.getElementById("main_group")
-		group.innerHTML = "";
-	}
-	else
-	{
-		group = document.createElementNS(svgns, "g");
-		group.setAttributeNS(null,"id","main_group");
-		this.svg.append(group)
-	}
-	var i = 0;
-	for (i=0;i<items;i++)
-	{
-		point = document.createElementNS(svgns, "circle");
-		point.setAttributeNS(null,"id","p_"+i);
-		point.setAttributeNS(null,"cx",0);
-		point.setAttributeNS(null,"cy",0);
-		point.setAttributeNS(null,"r",document.getElementById("pt_size").value);
-		point.setAttributeNS(null,"fill","#d17");
-		point.setAttributeNS(null,"stroke","white");
-		group.append(point)
-	}
+Svg.prototype.init_graph = function(items) {
+  if (document.getElementById("main_group") != null) {
+    group = document.getElementById("main_group")
+    group.innerHTML = "";
+  } else {
+    group = document.createElementNS(svgns, "g");
+    group.setAttributeNS(null,"id","main_group");
+    this.svg.append(group)
+  }
+  let i = 0;
+  for (i=0;i<items;i++) {
+    point = document.createElementNS(svgns, "circle");
+    point.setAttributeNS(null,"id","p_"+i);
+    point.setAttributeNS(null,"cx",0);
+    point.setAttributeNS(null,"cy",0);
+    point.setAttributeNS(null,"r",
+      document.getElementById("pt_size").value);
+    point.setAttributeNS(null,"fill","#d17");
+    point.setAttributeNS(null,"stroke","white");
+    group.append(point)
+  }
 }
-Svg.prototype.osc = async function(data,time)
-{
-	for (x in data)
-	{
-		point = document.getElementById("p_"+x)
-		point.setAttributeNS(null,"cx",x*(this.delta_x/data.length)-this.max_x);
-		point.setAttributeNS(null,"cy",((this.delta_y/256)*data[x]-this.max_y)*0.8);
-	}
+Svg.prototype.osc = async function(data,time) {
+  for (x in data) {
+    point = document.getElementById("p_"+x)
+    point.setAttributeNS(null,"cx",
+      x*(this.delta_x/data.length)-this.max_x);
+    point.setAttributeNS(null,"cy",
+      ((this.delta_y/256)*data[x]-this.max_y)*0.8);
+  }
 }
-var svg = new Svg(-500,-500,500,500);
-function incr_wait(i,t,rand=false)
-{
-	t = (rand) ? Math.floor(t+2*t*Math.random()):t;
-	return new Promise(function(resolve,reject){
-		setTimeout(function(){
-			resolve(i+1);
-		},t)
-	})
+function incr_wait(i,t,rand=false) {
+  t = (rand) ? Math.floor(t+2*t*Math.random()):t;
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve(i+1);
+    },t)
+  })
 }
